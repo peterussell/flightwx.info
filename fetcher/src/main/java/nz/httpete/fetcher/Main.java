92b3ff4b91687f379da84c65baa06a76c56f84cd
Pete Russell <pinkbatz@gmail.com>
2022-07-11 22:46:50 +1200

    Enabled Java debug port.

--- fetcher/src/main/java/nz/httpete/fetcher/Main.java
+++ fetcher/src/main/java/nz/httpete/fetcher/Main.java
@@ -1,7 +1,11 @@
 package nz.httpete.fetcher;
 
-public class Fetcher {
+    public class Fetcher {
     public static void main(String[] args) {
         System.out.println("Hello from the world's newest Java container!");
+    }
+
+    private void UpdateSectionals() {
+
     }
 }
