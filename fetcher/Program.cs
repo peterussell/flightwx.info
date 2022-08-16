b2cb1c0e0909b361440d60c7b51b698d449fcf71
Pete Russell <pinkbatz@gmail.com>
2022-08-16 13:45:50 +1200

    Added FlightWX solution file. Added fetcher project and docker build.

---
+++ fetcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlightWX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Starting fetcher...");
+        }
+    }
+}
