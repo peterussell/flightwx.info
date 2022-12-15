78501af38c7a053953125509b7f94079459fc427
Pete Russell <pinkbatz@gmail.com>
2022-12-15 22:51:28 +1300

    Added tailwindcss.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,15 +3,17 @@ import LandingPage from "pages/landing";
 import LoginPage from "pages/login";
 import { NavBar } from "features/navbar/components";
 
+import "./App.css";
+
 const App = () => {
   return (
-    <>
+    <div className="app-container">
       <NavBar />
       <Routes>
         <Route path="login" element={<LoginPage />} />
         <Route index element={<LandingPage />} />
       </Routes>
-    </>
+    </div>
   );
 };
 export default App;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
