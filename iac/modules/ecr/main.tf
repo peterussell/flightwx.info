59970f8fb9ae28b2bba24110ed2c241717d3067a
Pete Russell <pinkbatz@gmail.com>
2023-02-06 17:00:06 +1300

    Remove web ECR repository (plan to deploy to s3 bucket)

--- iac/modules/ecr/main.tf
+++ iac/modules/ecr/main.tf
@@ -10,16 +10,3 @@ resource "aws_ecr_repository" "fetcher" {
     Environment = "${var.environment}"
   }
 }
-
-resource "aws_ecr_repository" "web" {
-  name = "fetcher"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  tags = {
-    Environment = "${var.environment}"
-  }
-}
