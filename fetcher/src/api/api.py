1d2b7d25c51d4140d1d8e1962f900b661524e4a7
Pete Russell <pinkbatz@gmail.com>
2023-02-15 18:35:52 +1300

    Get FAA req/response working

--- fetcher/src/api/api.py
+++ fetcher/src/api/api.py
@@ -1,9 +1,27 @@
 import os
 from dotenv import load_dotenv
+import urllib3
+import json
+from urllib3 import HTTPResponse
+
+from api.http_verbs import HttpVerbs
 
 load_dotenv()
 
 base_url = os.environ["FAA_BASE_URL"]
+http = urllib3.PoolManager()
+
+def _full_url(path: str) -> str:
+    return f"{base_url}/{path}"
+
+def _get_response_body(response: HTTPResponse) -> any:
+    # return json.loads(response.data.decode("utf-8"))
+    return response.data.decode("utf-8")
 
 def get(path: str) -> None:
     print(f"Getting path {base_url}/{path}")
+    res: HTTPResponse = http.request(HttpVerbs.GET.value, _full_url(path))
+
+    print(f"API response status: {res.status}")
+    print("Response data")
+    print(_get_response_body(res))
---
+++ fetcher/src/api/http_verbs.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class HttpVerbs(Enum):
+    GET = "GET"
+    PUT = "PUT"
+    POST = "POST"
+    DELETE = "DELETE"
