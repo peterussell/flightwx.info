ad4994ca9a2d1382e0f716196a28ee7d7c434b26
Pete Russell <pinkbatz@gmail.com>
2023-02-20 09:41:51 +1300

    Chart edition response parsing works

--- fetcher/src/api/faa_api.py
+++ fetcher/src/api/faa_api.py
@@ -6,76 +6,72 @@ import xml.etree.ElementTree as ET
 
 import api.api as api
 import filesystem
+
+from models.chart_edition import ChartEdition
 from models.chart_type import ChartType
 from models.geoname import Geoname
 
 __all__ = ["update_charts"]
 
-SECTIONAL_URL = "vfr/sectional"
-SECTIONAL_CHART_URL = f"{SECTIONAL_URL}/chart"
-SECTIONAL_INFO_URL = f"{SECTIONAL_URL}/info"
+# Namespaces for the FAA XML docs
+# NB. there may be a better way to extract these from the doc rather than hard coding them
+NS = { "product_set": "http://arpa.ait.faa.gov/arpa_response" }
 
-
-
-def _get_sectional_info(geoname: Geoname) -> None:
-    api.get(f"{SECTIONAL_INFO_URL}?geoname={geoname.value}")
 
 def _get_vfr_chart_url(chart_type: ChartType, geoname: Geoname) -> str:
     return f"vfr/{chart_type.value}/chart?geoname={geoname.value}&edition=current&format=tiff"
 
-def _get_chart_meta(chart_type: ChartType, geoname: Geoname) -> None:
-    # res: HTTPResponse = api.get(_get_vfr_chart_url(chart_type, geoname))
 
-    # tmp - test parsing using a string
-    tmp_data = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><productSet xmlns="http://arpa.ait.faa.gov/arpa_response"><status code="200" message="OK"/><edition geoname="Chicago" editionName="CURRENT" format="TIFF"><editionDate>12/29/2022</editionDate><editionNumber>114</editionNumber><product productName="SECTIONAL" url="https://aeronav.faa.gov/visual/12-29-2022/sectional-files/Chicago.zip"/></edition></productSet>'
-
-    ns = {"product_set": "http://arpa.ait.faa.gov/arpa_response"}
+def _get_chart_edition(chart_type: ChartType, geoname: Geoname) -> None:
+    url = _get_vfr_chart_url(chart_type, geoname)
+    res: HTTPResponse = api.get(url)
     
-    root = ET.fromstring(tmp_data)
-    edition = root.find("product_set:edition", ns)
+    if (res.status != 200):
+        raise RuntimeError(f"Received a {res.status} response from {url}")
 
-    edition_attrib = edition.attrib
-    # edition_date_attrib = root.find("product_set:editionDate", ns) # not working
+    chart_edition = _parse_chart_edition(res)
 
-    # this is working!
-    geoname = edition_attrib["geoname"]
-    edition_name = edition_attrib["editionName"]
-    format = edition_attrib["format"]
-
-    # edition_date = edition_date_attrib.text # not working
-
-    print(edition_date_attrib)
-
-    print(f"Geoname: {geoname}")
-    print(f"Edition name: {edition_name}")
-    print(f"Format: {format}")
-    # print(f"Edition date: {edition_date}") # not working
+    # tmp - testing
+    print(str(chart_edition))
 
 
+# TODO: test
+def _parse_chart_edition(res: HTTPResponse) -> ChartEdition:
+    root = ET.fromstring(res.data.decode("utf-8"))
+    edition = root.find("product_set:edition", NS)
 
-    # if (res.status == 200):
-    #     body = res.data.decode("utf-8")
-    #     print(body) # tmp
-    #     root = ET.fromstring(body)
+    # Geoname, edition name, and format are attributes of <edition>
+    geoname = edition.attrib["geoname"]
+    edition_name = edition.attrib["editionName"]
+    file_format = edition.attrib["format"]
 
-    #     for child in root:
-    #         print(child.tag)
+    # Edition date, number, and product are children of <edition>
+    edition_date = edition.find("product_set:editionDate", NS).text
+    edition_number = edition.find("product_set:editionNumber", NS).text
+
+    # Product name and URL are attributes of <product>
+    product = edition.find("product_set:product", NS)
+    product_name = product.attrib["productName"]
+    product_url = product.attrib["url"]
+
+    chart_edition = ChartEdition(
+        geoname,
+        edition_name,
+        file_format,
+        edition_date,
+        edition_number,
+        product_name,
+        product_url
+    )
+    
+    return chart_edition
 
 
 def update_sectional_charts() -> None:
     print("Updating sectional charts")
     for chart_name in Geoname:
         if not filesystem.is_chart_current(chart_name):
-            _get_chart_meta(ChartType.SECTIONAL, chart_name)
-
-
-
-
-
-
-
-
-
+            _get_chart_edition(ChartType.SECTIONAL, chart_name)
 
 
 def update_terminal_area_charts() -> None:
--- fetcher/src/models/chart_edition.py
+++ fetcher/src/models/chart_edition.py
@@ -2,10 +2,43 @@
 Contains a ChartEdition class, representing an edition/release of an
 aeronautical chart.
 """
+from datetime import datetime
+
+from models.geoname import Geoname
 
 class ChartEdition:
     """Represents an edition/release of an aeronautical chart."""
-    def __init__(self, edition_date, zip_url, pdf_url):
-        self.edition_date = edition_date
-        self.zip_url = zip_url
-        self.pdf_url = pdf_url
+    def __init__(
+        self,
+        geoname: Geoname,
+        edition_name: str,
+        file_format: str,
+        edition_date: str, # TODO: date type?
+        edition_number: int,
+        product_name: str,
+        product_url: str
+        ):
+        self.geoname = geoname
+        self.edition_name = edition_name
+        self.file_format = file_format
+        self.edition_date = datetime.strptime(edition_date, "%m/%d/%Y")
+        self.edition_number = edition_number
+        self.product_name = product_name
+        self.product_url = product_url
+
+    def __str__(self):
+        attrib = {
+            "Geoname": self.geoname,
+            "Edition name" : self.edition_name,
+            "File format": self.file_format,
+            "Edition date": datetime.strftime(self.edition_date, "%m/%d/%Y"),
+            "Edition number" : self.edition_number,
+            "Product name" : self.product_name,
+            "URL": self.product_url,
+        }
+
+        res = ""
+        for k,v in attrib.items():
+            res += f"{k}: {v}\n"
+
+        return res
